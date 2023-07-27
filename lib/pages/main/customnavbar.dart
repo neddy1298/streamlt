ac593ea2f77c6b5b38bae0b68a0418326b35a4a2
Neddy AP <neddy1298@gmail.com>
2023-07-27 14:00:00 +0700

    Custom bottom nav bar for main page

---
+++ lib/pages/main/customnavbar.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:streamlt/pages/main/category_page.dart';
+import 'package:streamlt/pages/main/home_page.dart';
+
+class CustomNavBar extends StatelessWidget {
+  const CustomNavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 65,
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      decoration: const BoxDecoration(
+        color: Color(0xFF292B37),
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(25),
+          topRight: Radius.circular(25),
+        )
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => HomePage()),
+              );
+            },
+            child: const Icon(
+              Icons.home,
+              size: 35,
+              color: Colors.white,
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const CategoryPage()),
+              );
+            },
+            child: const Icon(
+              Icons.category,
+              size: 35,
+              color: Colors.white,
+            ),
+          ),
+          InkWell(
+            onTap: (){},
+            child: const Icon(
+              Icons.favorite_border,
+              size: 35,
+              color: Colors.white,
+            ),
+          ),
+          InkWell(
+            onTap: (){},
+            child: const Icon(
+              Icons.person,
+              size: 35,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
