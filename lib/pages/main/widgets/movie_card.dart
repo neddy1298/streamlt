d9a180fc9f4dace7be5e77c1dbf9b2b6cd823bee
Neddy AP <neddy1298@gmail.com>
2023-08-01 19:12:41 +0700

    optimize code to make it simple/shorter

---
+++ lib/pages/main/widgets/movie_card.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:streamlt/components/constants.dart';
+import 'package:streamlt/models/movie.dart';
+import 'package:streamlt/pages/main/movie_page.dart';
+
+class MovieCard extends StatelessWidget {
+  final String title;
+  final double? customSize;
+  final AsyncSnapshot<List<Movie>> snapshot;
+
+  const MovieCard({
+    required this.title,
+    required this.snapshot,
+    this.customSize,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final movies = snapshot.data;
+
+    if (movies == null || movies.isEmpty) {
+      return Center(
+        child: Text('No $title'),
+      );
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Text(
+                'See All',
+                style: const TextStyle(
+                  color: Colors.white54,
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 10),
+        SizedBox(
+          height: customSize ?? 250, // Use customSize when provided, or default to 250
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: movies!.length,
+            itemBuilder: (context, index) {
+              return _buildMovieCard(context, movies[index]);
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildMovieCard(BuildContext context, Movie movie) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => MoviePage(
+              movie: movie,
+              movieId: movie.id,
+            ),
+          ),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 4.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Image.network(
+            '${Constants.imagePath}${movie.poster_path}',
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
