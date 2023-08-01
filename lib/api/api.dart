6fdf572db5db80527055e5e6176e87a70e92646c
Neddy AP <neddy1298@gmail.com>
2023-08-01 19:11:59 +0700

    simply api code

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -4,25 +4,11 @@ import 'package:streamlt/models/movie.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
-  static const upcomingMovieUrl =
-      'https://api.themoviedb.org/3/movie/upcoming?api_key=${Constants.apiKey}';
-  static const newMovieUrl =
-      'https://api.themoviedb.org/3/movie/now_playing?api_key=${Constants.apiKey}';
-  static const popularMovieUrl =
-      'https://api.themoviedb.org/3/movie/popular?api_key=${Constants.apiKey}';
+  static const getMovieUrl = 'https://api.themoviedb.org/3/movie/';
 
-  Future<List<Movie>> getUpcomingMovies() async {
-    final response = await http.get(Uri.parse(upcomingMovieUrl));
-    if (response.statusCode == 200) {
-      final decodedData = jsonDecode(response.body)['results'] as List;
-      return decodedData.map((movie) => Movie.fromJson(movie)).toList();
-    } else {
-      throw Exception('Something happened');
-    }
-  }
-
-  Future<List<Movie>> getNewMovies() async {
-    final response = await http.get(Uri.parse(newMovieUrl));
+  Future<List<Movie>> getMovies(String movieType) async {
+    final response = await http
+        .get(Uri.parse('$getMovieUrl/$movieType?api_key=${Constants.apiKey}'));
     if (response.statusCode == 200) {
       final decodedData = jsonDecode(response.body)['results'] as List;
       return decodedData.map((movie) => Movie.fromJson(movie)).toList();
@@ -42,11 +28,12 @@ class Api {
     }
   }
 
-  Future<List<Movie>> getPopularMovies() async {
-    final response = await http.get(Uri.parse(popularMovieUrl));
+  Future<Movie> getMovie(String movieType) async {
+    final response = await http
+        .get(Uri.parse('$getMovieUrl/$movieType?api_key=${Constants.apiKey}'));
     if (response.statusCode == 200) {
       final decodedData = jsonDecode(response.body)['results'] as List;
-      return decodedData.map((movie) => Movie.fromJson(movie)).toList();
+      return Movie.fromJson(decodedData[0]);
     } else {
       throw Exception('Something happened');
     }
